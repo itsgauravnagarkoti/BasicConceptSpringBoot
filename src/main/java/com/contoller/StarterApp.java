1f5a7aeccdc8735c76ea0c7489756946affe1301
Gaurav Singh <devsinghgaurav.1144@gmail.com>
2025-05-15 15:53:14 +0530

    Change by default application.properties file location and adding property source annotation to grap the new location of application properties--new commit

--- src/main/java/com/contoller/StarterApp.java
+++ src/main/java/com/contoller/StarterApp.java
@@ -6,17 +6,25 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 
 //@EnableAutoConfiguration
 //@ComponentScan /// scaning all contollers 
 @SpringBootApplication
+@PropertySource("/Propfolder/application.properties")
 public class StarterApp {
 	
 	public static void main(String[] args) {
 		
 		// short form
 		
-		SpringApplication.run(StarterApp.class, args);
+		ConfigurableApplicationContext config = SpringApplication.run(StarterApp.class, args);
+		
+		MyApp app = config.getBean(MyApp.class);
+		
+		System.out.println(app.getAppliactionName());
+		System.out.println(app.getVersion());
+		System.out.println(app.getDescripition());
 		
 //		SpringApplication sa = new SpringApplication(StarterApp.class);
 //		sa.run(args);
